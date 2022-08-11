443c1f980f8913219a7ede0d62aca44e034f2dda
jprl <stifs.jp@hotmail.com>
2022-08-11 14:50:54 -0500

    encues

--- Controllers/Encuestaempresas.php
+++ Controllers/Encuestaempresas.php
@@ -1,34 +1,54 @@
-<?php 
+<?php
 
-	class encuestaempresas extends Controllers{
-		public function __construct()
-		{
-			parent::__construct();
-		}
-
-		public function encuestaempresas()
-		{
-			$data['page_id'] = 4;
-			$data['page_tag'] = "Estadisticas";
-			$data['page_title'] = "Página principal";
-			$data['page_name'] = "estadisticas";
-			$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
-			$this->views->getView($this,"encuestaempresas",$data);
-		}
-
-		public function perfilesacademicos()
-		{
-			$data['page_id'] = 4;
-			$data['page_tag'] = "Estadisticas";
-			$data['page_title'] = "Página principal";
-			$data['page_name'] = "estadisticas";
-			$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
-			$this->views->getView($this,"perfilesacademicos",$data);
-		}
-
-					
-
-		
-		
+class encuestaempresas extends Controllers
+{
+	public function __construct()
+	{
+		parent::__construct();
 	}
- ?>
+
+	public function encuestaempresas()
+	{
+		$data['page_id'] = 4;
+		$data['page_tag'] = "Estadisticas";
+		$data['page_title'] = "Página principal";
+		$data['page_name'] = "estadisticas";
+		$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
+		$this->views->getView($this, "encuestaempresas", $data);
+	}
+
+	public function perfilesacademicos()
+	{
+		$data['page_id'] = 4;
+		$data['page_tag'] = "Estadisticas";
+		$data['page_title'] = "Página principal";
+		$data['page_name'] = "estadisticas";
+		$data['page_content'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quis. Perspiciatis repellat perferendis accusamus, ea natus id omnis, ratione alias quo dolore tempore dicta cum aliquid corrupti enim deserunt voluptas.";
+		$this->views->getView($this, "perfilesacademicos", $data);
+	}
+
+
+	//insertar y actualizar los Banners
+	public function set()
+	{
+		if ($_POST) {
+			if (empty($_POST['pregunta1'])) {
+				$arrResponse = array("status" => false, "msg" => 'Datos incorrectos en la Encuesta.');
+			} else {
+				$pregunta1 = intval($_POST['pregunta1']);
+				$pregunta2 = intval($_POST['pregunta2']);
+				$pregunta3 = intval($_POST['pregunta3']);
+
+				$insert = $this->model->register($pregunta1, $pregunta2, $pregunta3);	
+
+				if ($insert > 0) {
+					$arrResponse = array('status' => true, 'msg' => 'La Encuesta fue llenada con éxito!.');
+				} else {
+					$arrResponse = array("status" => false, "msg" => 'No es posible almacenar los datos de la Encuesta.');
+				}
+			}
+			echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
+		}
+		die();
+	}
+}
